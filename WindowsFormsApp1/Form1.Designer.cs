3e79ea6f253a348c80a3a053bfa53b35447df2ed
amirmohammad ramezani <amirmohammadramezani1385@gmail.com>
2021-04-30 13:48:27 +0430

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblinteryourname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnsayhello = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblinteryourname
+            // 
+            this.lblinteryourname.AutoSize = true;
+            this.lblinteryourname.Location = new System.Drawing.Point(12, 20);
+            this.lblinteryourname.Name = "lblinteryourname";
+            this.lblinteryourname.Size = new System.Drawing.Size(90, 13);
+            this.lblinteryourname.TabIndex = 0;
+            this.lblinteryourname.Text = "Enter your name :";
+            this.lblinteryourname.MouseLeave += new System.EventHandler(this.lblinteryourname_MouseLeave);
+            this.lblinteryourname.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblinteryourname_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // btnsayhello
+            // 
+            this.btnsayhello.Location = new System.Drawing.Point(19, 62);
+            this.btnsayhello.Name = "btnsayhello";
+            this.btnsayhello.Size = new System.Drawing.Size(71, 33);
+            this.btnsayhello.TabIndex = 2;
+            this.btnsayhello.Text = "say Hello";
+            this.btnsayhello.UseVisualStyleBackColor = true;
+            this.btnsayhello.Click += new System.EventHandler(this.btnsayhello_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(96, 62);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(71, 33);
+            this.btnclear.TabIndex = 3;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(173, 62);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(71, 33);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(43, 174);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 13);
+            this.lblresult.TabIndex = 5;
+            this.lblresult.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 215);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnsayhello);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblinteryourname);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = " MR.GRAY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblinteryourname;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnsayhello;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string V = "hello ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void لغو_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Next_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void lblinteryourname_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblinteryourname.ForeColor = Color.Red;
+        }
+
+        private void lblinteryourname_MouseLeave(object sender, EventArgs e)
+        {
+            lblinteryourname.ForeColor = Color.Black;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsayhello_Click(object sender, EventArgs e)
+        {
+
+            string s;
+            s = "hello" + textBox1.Text;
+            lblresult.Text = s;
+
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            lblresult.Text = "";
+            textBox1.Focus();
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                btnsayhello.Enabled = false;
+            else
+                btnsayhello.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                if (textBox1.Text == "")
+                    btnsayhello.Enabled = false;
+                else
+                    btnsayhello.Enabled = true;
+            
+        }
+
+        }
+    }
+}
